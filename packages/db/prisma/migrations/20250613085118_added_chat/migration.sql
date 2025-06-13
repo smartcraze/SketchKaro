d35dea3e72dd9252c2a0ab577af67d2be111f187
suraj vishwakarma <smartcraze354@gmail.com>
2025-06-13 14:21:42 +0530

    db

---
+++ packages/db/prisma/migrations/20250613085118_added_chat/migration.sql
@@ -0,0 +1,56 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `Room` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Room` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `Room` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[slug]` on the table `Room` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `adminId` to the `Room` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `slug` to the `Room` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Room" DROP CONSTRAINT "Room_userId_fkey";
+
+-- DropIndex
+DROP INDEX "User_username_key";
+
+-- AlterTable
+ALTER TABLE "Room" DROP COLUMN "name",
+DROP COLUMN "updatedAt",
+DROP COLUMN "userId",
+ADD COLUMN     "adminId" TEXT NOT NULL,
+ADD COLUMN     "slug" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+DROP COLUMN "username",
+ADD COLUMN     "email" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "roomId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Room_slug_key" ON "Room"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Room" ADD CONSTRAINT "Room_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
