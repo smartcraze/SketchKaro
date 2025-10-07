ca8ca212ef54a986b865a81ad52c12d9b4df19e7
suraj vishwakarma <smartcraze354@gmail.com>
2025-10-07 15:10:48 +0530

    feat: update backend and websocket URLs for production environment

--- apps/web/Config.ts
+++ apps/web/Config.ts
@@ -1,4 +1,12 @@
-// export const HTTP_BACKEND = "https://sketchkaro-jovi.onrender.com";
-// export const WS_URL = "https://sketchkaro-ws-backend.onrender.com";
+/* 
+for development only
+
 export const HTTP_BACKEND = "http://localhost:3001";
+
 export const WS_URL = "http://localhost:8080";
+ */
+
+
+export const HTTP_BACKEND = "https://sketchkaro-http-backend.onrender.com";
+
+export const WS_URL = "https://sketchkaro-ws-backend-bopw.onrender.com";
